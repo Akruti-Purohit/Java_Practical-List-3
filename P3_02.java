d1bc0a883d98902a2bc3c89ec47bf156583af2d3
Akruti-Purohit <159265800+Akruti-Purohit@users.noreply.github.com>
2024-03-29 23:43:23 +0530

    commit

--- P3_02.java
+++ P3_02.java
@@ -1,15 +1,25 @@
 import java.util.*;
 
-public class P3_02{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter year:");
-		int year = sc.nextInt();
+class Leapyear{
+	int year;
+	Leapyear(int year){
+		this.year = year;
+	}
+}
+class Check{
+	Check(int year){
+		super(year);
 		if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
 			System.out.println("This is a leap year.");
 		}
 		else{
 			System.out.println("This is not a leap year.");
 		}
+	}
+}
+
+public class P3_02{
+	public static void main(String args[]){
+		Check y1 = new Check(2000);
 	}
 }
